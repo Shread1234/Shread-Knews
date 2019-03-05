d2ef0e5cafd7fd538a0cfa122998e2454c67da3f
shread1234 <shread1234@hotmail.co.uk>
2019-03-05 10:20:07 +0000

    Added Home and Articles paths

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
-import NavBar from './components/navBar';
+import { Router } from '@reach/router';
+import Articles from './components/Articles';
+import Home from './components/Home';
 
 class App extends Component {
+  state = {
+    currentUser: ''
+  };
+
+  handleUserChange = (event) => {
+    event.preventDefault();
+    const value = event.target.value;
+    this.setState({ currentUser: value });
+  };
+
+  handleClick = (event) => {
+    event.preventDefault();
+  };
   render() {
     return (
       <div className="App">
-        <NavBar />
-        <h1>Shread Knews</h1>
+        <Router>
+          <Home path="/" />
+          <Articles path="/articles" />
+        </Router>
       </div>
     );
   }
