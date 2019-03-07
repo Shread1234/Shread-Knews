b4a54f63dfc404dd7d9295cb5d9b2c5abbd6bb98
shread1234 <shread1234@hotmail.co.uk>
2019-03-07 21:02:25 +0000

    logged in users can now upvote and downvote articles

--- src/Utils/Axios.js
+++ src/Utils/Axios.js
@@ -2,6 +2,15 @@ import axios from 'axios';
 
 const baseURL = 'http://shread-knews.herokuapp.com/api/';
 
+const sort_by = {
+  'Oldest-Newest': 'sort_by=created_at&order=asc',
+  'Newest-Oldest': 'sort_by=created_at&order=desc',
+  'Comments Descending': 'sort_by=comment_count&order=desc',
+  'Comments Ascending': 'sort_by=comment_count&order=asc',
+  'Votes Descending': 'sort_by=votes&order=desc',
+  'Votes Ascending': 'sort_by=votes&order=asc'
+};
+
 export function getAllArticles(topicSlug) {
   return topicSlug === undefined
     ? axios.get(`${baseURL}articles`)
@@ -20,15 +29,6 @@ export function getCommentsByArticle(article_id) {
   return axios.get(`${baseURL}articles/${article_id}/comments`);
 }
 
-const sort_by = {
-  'Oldest-Newest': 'sort_by=created_at&order=asc',
-  'Newest-Oldest': 'sort_by=created_at&order=desc',
-  'Comments Descending': 'sort_by=comment_count&order=desc',
-  'Comments Ascending': 'sort_by=comment_count&order=asc',
-  'Votes Descending': 'sort_by=votes&order=desc',
-  'Votes Ascending': 'sort_by=votes&order=asc'
-};
-
 export function allArticlesOrder(value) {
   return axios.get(`${baseURL}articles?${sort_by[value]}`);
 }
@@ -41,4 +41,8 @@ export function topicArticlesOrder(value, topic) {
 
 export function userCheck(user) {
   return axios.get(`${baseURL}users/${user}`);
+}
+
+export function articleVote(articleId, vote) {
+  return axios.patch(`${baseURL}articles/${articleId}`, { inc_votes: vote });
 }
--- src/Utils/SingleArticleViewer.jsx
+++ src/Utils/SingleArticleViewer.jsx
@@ -1,41 +1,77 @@
 import React from 'react';
 import { Link } from '@reach/router';
 import Comments from '../components/Comments';
+import { articleVote } from '../Utils/Axios';
 
-export default function SingleArticleViewer(props) {
-  const { article, user } = props;
-  return (
-    <ul>
-      {article !== null &&
-        [article].map((article) => (
-          <div key={article.article_id}>
-            <br />
-            <br />
-            <br />
-            <h1>{article.title}</h1>
-            <br />
-            By: <Link to={`/users/${article.author}`}>
-              {' '}
-              {article.author}
-            </Link>{' '}
-            &nbsp;&nbsp;&nbsp;&nbsp;Posted:{' '}
-            {new Date(article.created_at).toUTCString()}{' '}
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Topic:{' '}
-            <Link to={`/articles/topic/${article.topic}`}>{article.topic}</Link>
-            <p>{article.body}</p>
-            <p>
-              Comments: {article.comment_count} &nbsp; Votes: {article.votes}
-            </p>
-            {user && (
-              <div>
-                <button id="articleUpVote">Up Vote</button>
-                <button id="articleDownVote">Down Vote</button>
-              </div>
-            )}
-            <br />
-            <Comments article_id={article.article_id} />
-          </div>
-        ))}
-    </ul>
-  );
+export default class SingleArticleViewer extends React.Component {
+  state = {
+    voteChange: 0
+  };
+
+  handleVoteChange = (event) => {
+    const articleId = this.props.article.article_id;
+    const vote = Number(event.target.value);
+    articleVote(articleId, vote);
+    this.setState((prevState) => {
+      return {
+        voteChange: prevState.voteChange + vote
+      };
+    });
+  };
+
+  render() {
+    const { article, user } = this.props;
+    const { voteChange } = this.state;
+    return (
+      <ul>
+        {article !== null &&
+          [article].map((article) => (
+            <div key={article.article_id}>
+              <br />
+              <br />
+              <br />
+              <h1>{article.title}</h1>
+              <br />
+              By: <Link to={`/users/${article.author}`}>
+                {' '}
+                {article.author}
+              </Link>{' '}
+              &nbsp;&nbsp;&nbsp;&nbsp;Posted:{' '}
+              {new Date(article.created_at).toUTCString()}{' '}
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Topic:{' '}
+              <Link to={`/articles/topic/${article.topic}`}>
+                {article.topic}
+              </Link>
+              <p>{article.body}</p>
+              <p>
+                Comments: {article.comment_count} &nbsp; Votes:{' '}
+                {article.votes + voteChange}
+              </p>
+              {user && (
+                <div>
+                  <button
+                    disabled={voteChange === 1}
+                    onClick={this.handleVoteChange}
+                    id="articleUpVote"
+                    value="1"
+                  >
+                    Up Vote
+                  </button>
+                  <button
+                    disabled={voteChange === -1}
+                    onClick={this.handleVoteChange}
+                    id="articleDownVote"
+                    value="-1"
+                  >
+                    Down Vote
+                  </button>
+                </div>
+              )}
+              <br />
+              <Comments article_id={article.article_id} />
+            </div>
+          ))}
+      </ul>
+    );
+  }
 }
